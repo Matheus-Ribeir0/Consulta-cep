4c6657e6c6d9c3baf3c3cb04d9d3bdfb2e112c8c
Matheus-Ribeir0 <Matheus.tex02@gmail.com>
2023-04-13 23:26:16 -0300

    projeto rodando

--- consulta-cep/src/App.js
+++ consulta-cep/src/App.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
 import './App.css';
 
 function App() {
-  const { register, handleSubmit, setValue } = useForm();
   const [cepData, setCepData] = useState(null); // Estado para armazenar os dados do CEP
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const cep = formData.get('cep').replace(/\D/g, '');
     // Fazer o fetch dos dados do CEP aqui, e atualizar o estado cepData com os dados obtidos
-    const cep = data.cep.replace(/\D/g, '');
     fetch(`https://viacep.com.br/ws/${cep}/json/`)
       .then(res => res.json())
       .then(data => {
@@ -20,9 +19,9 @@ function App() {
 
   return (
     <div className='form'>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={onSubmit}>
         <label>
-          <input className='input-cep' type='text' {...register('cep')} />
+          <input className='input-cep' type='text' name='cep' />
         </label>
         <button type='submit'>Enviar</button>
       </form>
